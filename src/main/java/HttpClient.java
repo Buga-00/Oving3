079677a10490a6c5f1772de0343955db310d2f45
Buga-00 <cemal.buga@hotmail.com>
2021-09-23 15:23:37 +0200

    shouldReadContentLength test (fails)

--- src/main/java/HttpClient.java
+++ src/main/java/HttpClient.java
@@ -62,4 +62,8 @@ public class HttpClient {
     public String getHeader(String headerName) {
         return headerFields.get(headerName);
     }
+
+    public int getContentLength() {
+        return 0;
+    }
 }
