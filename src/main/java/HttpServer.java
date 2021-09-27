32fa4eef13030bb31e4cb19dbde5b90794e7d397
Buga-00 <cemal.buga@hotmail.com>
2021-09-27 14:43:36 +0200

    shouldRespondWithRequestTargetIn404 test using long time

--- src/main/java/HttpServer.java
+++ src/main/java/HttpServer.java
@@ -16,7 +16,14 @@ public class HttpServer {
     private void handleClients(){
         try{
             Socket clientSocket = serverSocket.accept();
-            String responseBody = "HTTP/1.1 404 Not found\r\nContent-Length: 0\r\n\r\n";
+
+            String[] requestLine = HttpClient.readLine(clientSocket).split(" ");
+            String requestTarget = requestLine[1];
+            String responseText = "File not found: " + requestTarget;
+
+            String responseBody = "HTTP/1.1 404 Not found\r\n" +
+                    "Content-Length: " +  responseText.length() +  "\r\n" +
+                    "\r\n";
             clientSocket.getOutputStream().write(responseBody.getBytes());
         } catch (IOException e) {
             e.printStackTrace();
@@ -51,4 +58,7 @@ public class HttpServer {
         clientSocket.getOutputStream().write(responseBody.getBytes());
     }
 
+    public int getPort() {
+        return serverSocket.getLocalPort();
+    }
 }
