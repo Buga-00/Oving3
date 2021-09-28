19026668d0e3740e603d97b4f776f4db8719a42c
Buga-00 <cemal.buga@hotmail.com>
2021-09-28 13:34:14 +0200

    HttpServer main uses HttpServer

--- src/main/java/HttpServer.java
+++ src/main/java/HttpServer.java
@@ -43,31 +43,7 @@ public class HttpServer {
     }
 
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(8080);
-
-
-        Socket clientSocket = serverSocket.accept();
-
-        String requestLine = HttpClient.readLine(clientSocket);
-
-        System.out.println(requestLine);
-
-        String headerLine;
-        while (!(headerLine = HttpClient.readLine(clientSocket)).isBlank()){
-            System.out.println(headerLine);
-        }
-
-        String messageBody = "Hello World";
-        String contentType = "text/html";
-
-        String responseBody = "HTTP/1.1 200 OK\r\n" +
-                "Content-Length: " + messageBody.length() + "\r\n" +
-                "Content-type: " + contentType + "\r\n" +
-                "Connection: close\r\n" +
-                "\r\n" +
-                messageBody;
-
-        clientSocket.getOutputStream().write(responseBody.getBytes());
+        new HttpServer(8080);
     }
 
     public int getPort() {
