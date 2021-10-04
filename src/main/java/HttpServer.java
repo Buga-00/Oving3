a2c7b075bb9d90071ffc7c83165d2d41eb1c8551
Buga-00 <cemal.buga@hotmail.com>
2021-10-04 15:07:23 +0200

    Change of requirements: "Hello" need both first and last names refactor

--- src/main/java/HttpServer.java
+++ src/main/java/HttpServer.java
@@ -52,14 +52,7 @@ public class HttpServer {
         if (fileTarget.equals("/hello")) {
             String yourName = "world";
             if (query != null) {
-                Map<String, String> queryMap = new HashMap<>();
-
-                for (String queryParameter : query.split("&")) {
-                    int equalsPos = queryParameter.indexOf('=');
-                    String parameterName = queryParameter.substring(0, equalsPos);
-                    String parameterValue = queryParameter.substring(equalsPos+1);
-                    queryMap.put(parameterName, parameterValue);
-                }
+                Map<String, String> queryMap = parseRequestParameters(query);
 
                 yourName = queryMap.get("lastName") + ", " + queryMap.get("firstName");
             }
@@ -98,6 +91,17 @@ public class HttpServer {
                     responseText;
             clientSocket.getOutputStream().write(responseBody.getBytes());
         }
+    }
+
+    private Map<String, String> parseRequestParameters(String query) {
+        Map<String, String> queryMap = new HashMap<>();
+        for (String queryParameter : query.split("&")) {
+            int equalsPos = queryParameter.indexOf('=');
+            String parameterName = queryParameter.substring(0, equalsPos);
+            String parameterValue = queryParameter.substring(equalsPos+1);
+            queryMap.put(parameterName, parameterValue);
+        }
+        return queryMap;
     }
 
     private void writeOkResponse(Socket clientSocket, String responseText, String contentType) throws IOException {
